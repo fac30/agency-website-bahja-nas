ff857bf3db0c8edfcefc05800c05ddcfcb36d06e
Nas <n.choudhury1@live.co.uk>
2024-01-20 17:16:01 +0000

    saving prior of merging maiin update

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 const siteName  = 'Pawsitive Haven Adoption Agency';
 
 
+/* ----
+Menu
+----- */
 
+const toggleBtn = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleBtn.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+})
+
+
+/* -----
+Hidden Text
+----- */
+
+const toggleText = () => {
+    const additionalText = document.querySelector('.additional-text');
+    additionalText.classList.toggle('hiddenText');
+}
 
 
 /* -----
